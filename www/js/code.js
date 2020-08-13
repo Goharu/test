b486bf9e3feb93f1918dbde437e0d95b74270ca5
Goharu <68592124+Goharu@users.noreply.github.com>
2020-08-13 15:34:29 +0300

    Add files via upload

---
+++ www/js/code.js
@@ -0,0 +1,13 @@
+$(function () {
+   $(".lupa").click(function () {
+      $(".header_search_form").toggleClass("vision")
+   });
+   $(".closer").click(function () {
+      $(".header_search_form").removeClass("vision")
+   });
+   
+});
+
+
+
+
